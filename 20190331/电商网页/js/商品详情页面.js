390b78a40e54d08975fbfe83aea984cec7d539fa
13081156765 <48549418+13081156765@users.noreply.github.com>
2019-03-31 23:40:23 +0800

    Add files via upload

---
+++ 20190331/电商网页/js/商品详情页面.js
@@ -0,0 +1,44 @@
+$(function(){
+	$("#rise").html("<div class='rise1'><a href='睿乐首页.html'><p>珠地布机车外套</p></a></div>");
+	$("#main").html("<div id='carousel-example-generic' class='carousel slide' data-ride='carousel'>"+
+//          <!-- Wrapper for slides -->
+            "<div class='carousel-inner' role='listbox'>"+
+                "<div class='item active'>"+
+                    "<img src='../img/1 (1).png' alt='图片找不到了...'>"+
+                "</div>"+
+                "<div class='item'>"+
+                    "<img src='../img/1 (4).png' alt='图片找不到了...'>"+
+                "</div>"+
+                "<div class='item'>"+
+                    "<img src='../img/1 (5).png' alt='图片找不到了...'>"+
+                "</div>"+
+                "<div class='item'>"+
+                    "<img src='../img/1 (2).png' alt='图片找不到了...'>"+
+                "</div>"+
+                "<div class='item'>"+
+                    "<img src='../img/1 (3).png' alt='图片找不到了...'>"+
+                "</div>"+
+//          <!-- Controls -->+
+            "<a class='left carousel-control' href='#carousel-example-generic' role='button' data-slide='prev'>"+
+                "<span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>"+
+                "<span class='sr-only'>Previous</span>"+
+            "</a>"+
+            "<a class='right carousel-control' href='#carousel-example-generic' role='button' data-slide='next'>"+
+                "<span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>"+
+                "<span class='sr-only'>Next</span>"+
+            "</a>"+
+            "</div>"+
+            "<br />"+
+	    "<div class='chinese'><b>2019年ZARA新款 珠地布机车外套</b>￥459.00</div>"+
+	    "<div class='introduce'>颜色：红色-0706/490<br /><br />轻便外套，颈带领口，长袖，袖口饰有拉链。胸部饰有拉链口袋，胯部设计有饰边口袋。正面拉链闭合。<br /><br />模特身高。189CM 大码</div>"+
+	    "<hr />"+
+	    "<div class='ol'><ul><br />"+
+	    "<li>S(175/92A)</li><br />"+
+	    "<li>M(180/96A)</li><br />"+
+	    "<li>L(180/100A)</li><br />"+
+	    "<li>XL(185/104A)</li><br />"+
+	    "</ul></div>"+
+	    "<hr />");
+	$("#cl").html("<div class='shop'><a href='#'><input type='submit' value='加入购物车'/></a></div>"    
+	);
+})
